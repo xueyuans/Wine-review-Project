c9104d37c77116e3e268910e66e2fdcdef27119a
xueyuans <shi.xuey@husky.neu.edu>
2018-11-29 13:13:43 -0800

    first version
    Linear regression

--- train_model.py
+++ train_model.py
@@ -4,17 +4,22 @@ To train this model, in your terminal:
 """
 
 from sklearn.externals import joblib
-from sklearn import datasets
-from sklearn.tree import DecisionTreeClassifier
+import pandas as pd
+from sklearn.metrics import r2_score
+from sklearn import linear_model
 
-print('Loading the iris dataset')
-iris = datasets.load_iris()
-X = iris.data
-y = iris.target
+print('Loading the wine-review dataset')
+df = pd.read_csv('C:\AI\\wine-reviews\winemag-data-130k-v2.csv', na_values="?")
+data = df[["points", "price"]]
 
-print('Training a Decision Tree classifier')
-clf = DecisionTreeClassifier()
-clf.fit(X, y)
+print('Training a LinearRegression')
+data = data.dropna()
+ # Treat the column price as our predictive objective
+data_y = data["price"]
+ # All other columns will be used as features when training our model
+data_x = data.drop(["price"], axis=1)
+regr = linear_model.LinearRegression()
+regr.fit(data_x, data_y)
 
 print('Exporting the trained model')
-joblib.dump(clf, 'model/iris_classifier.joblib')
+joblib.dump(regr, 'model/wine-review.joblib')
--- wine_regression_api.py
+++ wine_regression_api.py
@@ -1,6 +1,6 @@
 """
 To run this app, in your terminal:
-> python iris_classification_api.py
+> python wine_regression_api.py
 """
 import connexion
 from sklearn.externals import joblib
@@ -10,37 +10,31 @@ app = connexion.FlaskApp(__name__, port=8080, specification_dir='swagger/')
 application = app.app
 
 # Load our pre-trained model
-clf = joblib.load('./model/iris_classifier.joblib')
+regr = joblib.load('./model/wine-review.joblib')
 
 # Implement a simple health check function (GET)
 def health():
     # Test to make sure our service is actually healthy
     try:
-        predict(1, 1, 1, 1)
+        predict(87)
     except:
         return {"Message": "Service is unhealthy"}, 500
 
     return {"Message": "Service is OK"}
 
 # Implement our predict function
-def predict(sepal_length, sepal_width, petal_length, petal_width):
+def predict(points):
     # Accept the feature values provided as part of our POST
     # Use these as input to clf.predict()
-    prediction = clf.predict([[sepal_length, sepal_width, petal_length, petal_width]])
-
-    # Map the predicted value to an actual class
-    if prediction[0] == 0:
-        predicted_class = "Iris-Setosa"
-    elif prediction[0] == 1:
-        predicted_class = "Iris-Versicolour"
-    else:
-        predicted_class = "Iris-Virginica"
+    prediction = regr.predict(points)
 
     # Return the prediction as a json
-    return {"prediction" : predicted_class}
+    if prediction[0] < 0:
+        prediction[0] = 0;
+    return prediction[0]
 
 # Read the API definition for our service from the yaml file
-app.add_api("iris_classification_api.yaml")
+app.add_api("wine_regression_api.yaml")
 
 # Start the app
 if __name__ == "__main__":
